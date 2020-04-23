2906e3604a98cfc04fb197764010fd58e857e0c7
Fronikateur <vilbert.pierre.84@gmail.com>
2020-04-23 13:04:43 +0200

    deuxieme

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.test
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,6 +13,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun ButtonSearch(button:View){
+        val textResearch = editText.text
 
     }
 }
